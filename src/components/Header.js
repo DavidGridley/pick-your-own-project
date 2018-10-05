5bda99bd6c775696407704e0940c982ab7433cec
David Gridley <davidgridley21@gmail.com>
2018-10-05 15:45:38 +0100

    more pre demo tweeks

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,17 @@ import React from 'react';
 class Header extends React.Component {
     constructor(){
         super();
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        window.location.reload();
     }
 
     render() {
         return (
             <div className="header">
-                <h1 className="header__title">Nonna</h1>
+                <h1 onClick={this.handleClick} className="header__title">Nonna</h1>
                 <h2 className="header__sub-title">A time honoured recipe collection</h2>
             </div>
         )
